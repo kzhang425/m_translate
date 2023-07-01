f4f1c328637e4f5874490ceb9a2a0e9ff81aaa66
kzhang425 <kzhang425@gmail.com>
2023-07-01 14:41:36 -0500

    comments

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use m_translate::*;
 use std::env;
 use std::result;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::path::Path;
 use std::fs::File;
 
@@ -22,9 +22,15 @@ fn open_to_buffer(arg: String) -> io::Result<String> {
     let size = file.read_to_string(&mut buffer)?;
     println!("{} bytes were read from file.", size);
 
-    Ok(buffer)
+    Ok(buffer) 
+}
 
-    
+// Mumps code files generally end in .mps, so make sure to have that extension
+fn write_to_file(input: String, file_path: &str) -> io::Result<()> {
+    let mut file = File::create(file_path)?;
+    write!(file, "{}", input);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -35,6 +41,11 @@ mod io_tests {
     fn file_read() {
         let s = open_to_buffer("test.txt".to_string()).unwrap();
         assert_eq!("Hello World".to_string(), s);
+    }
+
+    #[test]
+    fn file_write() {
+        let test_string = String::from("Hello World");
 
     }
 }
